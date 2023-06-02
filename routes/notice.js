4b545c524c58598999da7896abb14cb8c5801b88
mingi <oon418@gmail.com>
2023-06-02 15:56:26 +0900

    'edit_03:56'

--- routes/notice.js
+++ routes/notice.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql2");
 const multer = require("multer");
+const passport = require("passport");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 
